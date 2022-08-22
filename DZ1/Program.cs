0c0e853386722d3f15108118a55f9a561e4b0d48
BOBsoft <bobsoft_spb@mail.ru>
2022-08-22 14:33:27 +0300

    DZ1

---
+++ DZ1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2=Convert.ToInt32(Console.ReadLine());
+
+if(number1 > number2)
+{
+    Console.Write("Первое число ");
+    Console.Write(number1);
+    Console.Write(" больше второго ");
+    Console.Write(number2);
+}
+else
+{
+    Console.Write("Второе число ");
+    Console.Write(number2);
+    Console.Write(" больше первого ");
+    Console.Write(number1);
+}
